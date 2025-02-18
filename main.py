85a3144d8e44f02361bf8ef992a95f72424fec13
WDAhyhy <1485826483@qq.com>
2025-02-18 20:19:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from fabric import Connection
 from astrbot.api.message_components import *
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-@register("fish_countapiimg", "案板上的鹹魚", "计算服务器中有的图片数量", "1.0")
 host = "31.56.123.4"
 username = "root"
-
 conn = Connection(host=host, user=username, connect_kwargs={"password": "Qwer3866373"})
-print(result.stdout)
+@register("fish_countapiimg", "案板上的鹹魚", "计算服务器中有的图片数量", "1.0")
 class Countimg(Star):
     def __init__(self, context: Context):
         super().__init__(context)
