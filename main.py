caa22d14a0b0d50a1ccbc0481a5c44f8ef8f620a
WDAhyhy <1485826483@qq.com>
2025-02-18 20:30:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ class Countimg(Star):
     @filter.command("cimg")
     async def get_countimg(self, event: AstrMessageEvent):
         result = conn.run("ls -l /root/random-api/portrait/NON-H | wc -l", hide=True)
-        yield event.send(result.stdout)
+        yield event.result(result.stdout)
